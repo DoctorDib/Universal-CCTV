eb534bc0a767e43fef39bd6b38d37182650de748
James Dibnah <jamesdibnah@gmail.com>
2023-11-19 16:54:28 +0000

    Added build so I don't have to build on pi

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Navigate to the React app directory
-cd ./Client
+# cd ./Client
 
 # Install dependencies and build the React app
-npm install
-npm run build
+# npm install
+# npm run build
 
 # Start the React app with pm2
-pm2 start npm --name "Security Camera (Client)" -- start
+# pm2 start npm --name "Security Camera (Client)" -- start
 
 # Navigate to the Python app directory
-cd ../
 
 # Start the Python app with pm2
-pm2 start app.py --name "Securityh Camera (Server)"
+# pm2 start main.py --name "Securityh Camera (Server)"
